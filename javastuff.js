19b2ec20c0cd188591061e7333818868dd8c1d75
MitchellPriour <mitchellpriour@email.tamu.edu>
2016-10-27 07:08:31 -0500

    the final push?

--- javastuff.js
+++ javastuff.js
@@ -11,27 +11,27 @@ var filters = {
 };
 
 var urls = [
-    {"amazon":"https://www.amazon.com/EVGA-GeForce-Dual-Link-Graphics-06G-P4-2790-KR/dp/B00BIUKH04", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16814130897"},
+    {"amazon":"https://www.amazon.com/Radeon-128-Bit-Graphics-Cards-R7250-2GD5/dp/B01F3QEZ1U", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=9SIA1N84829645"}, /* Replace this */
     {"amazon":"https://www.amazon.com/Sapphire-Radeon-backplate-Graphics-11260-07-20G/dp/B01J1M4BZ2", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16814202224"},
     {"amazon":"https://www.amazon.com/EVGA-GeForce-Display-Graphics-02G-P4-3757-KR/dp/B00J0ISHMQ", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16814487028"},
-    {"amazon":"https://www.amazon.com/GeForce-GTX-1060-VCGGTX10606PB-CG-Graphics/dp/B01LOVVJ9E", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16814133631"},
+    {"amazon":"https://www.amazon.com/MSI-GAMING-RX-470-4G/dp/B01JS9F9K4", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16814137024"}, /* this one */
     {"amazon":"https://www.amazon.com/EVGA-GeForce-Support-Graphics-08G-P4-6173-KR/dp/B01GX5YWAO", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16814487248"},
-    {"amazon":"https://www.amazon.com/ASUS-GeForce-STRIX-Graphics-STRIX-GTX1080-A8G-GAMING/dp/B01K5F8MJK", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16814126116"},
+    {"amazon":"https://www.amazon.com/EVGA-GeForce-Support-Graphics-03G-P4-6162-KR/dp/B01KU2CIIY", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16814487263"}, /* this one */
     {"amazon":"https://www.amazon.com/2133MHz-Non-ECC-Desktop-HX421C14FBK2-16/dp/B00TY6A1LY", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16820104531"},
-    {"amazon":"https://www.amazon.com/Corsair-Vengeance-2400MHz-PC4-19200-Memory/dp/B00UVN2C8O", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16820233982"},
+    {"amazon":"https://www.amazon.com/Kingston-Technology-HyperX-HX421S13IBK2-16/dp/B014R8JRRW", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16820233982"},
     {"amazon":"https://www.amazon.com/Crucial-Ballistix-PC3-12800-240-Pin-BLS2KIT8G3D1609DS1S00/dp/B006YG9EEW", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=9SIA5752RR9050"},
     {"amazon":"https://www.amazon.com/Ballistix-Sport-Single-PC4-19200-288-Pin/dp/B01DPSQOO4", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16820156109"},
-    {"amazon":"https://www.amazon.com/Kingston-HyperX-FURY-1600MHz-DDR3/dp/B00J8E92M6", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=9SIA24G3RH4801"},
-    {"amazon":"https://www.amazon.com/GeIL-POTENZA-288-Pin-Desktop-GPR416GB2400C16DC/dp/B01EWBFZSE", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16820158024"},
+    {"amazon":"https://www.amazon.com/G-SKILL-Ripjaws-288-Pin-Desktop-F4-2400C15D-16GVR/dp/B013J7T5K6", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16820231888"},
+    {"amazon":"https://www.amazon.com/Corsair-Vengeance-2x8GB-2400MHz-Desktop/dp/B00EUPV2RQ", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16820233585"},
     {"amazon":"https://www.amazon.com/Intel-Unlocked-Skylake-Processor-BX80662I76700K/dp/B012M8LXQW", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16819117559"},
     {"amazon":"https://www.amazon.com/Intel-I7-6700-FC-LGA14C-Processor-BX80662I76700/dp/B0136JONG8", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16819117560"},
-    {"amazon":"https://www.amazon.com/AMD-FD6300WMHKBOX-FX-6300-6-Core-Processor/dp/B009O7YORK", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16819113286"},
-    {"amazon":"https://www.amazon.com/AMD-A6-5400K-Dual-Core-Processor-AD540KOKHJBOX/dp/B0095VPBFY", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16819113282"},
+    {"amazon":"https://www.amazon.com/Intel-i3-6100-Cache-Processor-BX80662I36100/dp/B015VPX2EO", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=2MN-0004-00002"},
+    {"amazon":"https://www.amazon.com/AMD-FD8350FRHKBOX-FX-8350-8-Core-Processor/dp/B009O7YUF6", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16819113282"},
     {"amazon":"https://www.amazon.com/AMD-8-Core-FX-8350-Processor-FD8350FRHKHBX/dp/B01F4ZOB3C", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16819113415"},
-    {"amazon":"https://www.amazon.com/Intel-Skylake-Desktop-Processor-BX80662I56600K/dp/B012M8M7TY", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16819117561"}
+    {"amazon":"https://www.amazon.com/Intel-Skylake-Desktop-Processor-BX80662I56600K/dp/B012M8M7TY", "newegg":"http://www.newegg.com/Product/Product.aspx?Item=N82E16819113284"}
 ];
 
-function product(brand, model, price, imageUrl, averageRating, reviews, specifications, id, amazonUrl, neweggUrl) {
+function product(brand, model, price, imageUrl, averageRating, reviews, specifications, id, amazonUrl, neweggUrl, index) {
     this.brand = brand;
 	this.model = model;
 	this.price = price;
@@ -44,16 +44,18 @@ function product(brand, model, price, imageUrl, averageRating, reviews, specific
 	this.showOnPage = true;
     this.amazonUrl = amazonUrl;
     this.neweggUrl = neweggUrl;
+    this.index = index;
 }
 
-window.onload = function() { 
-	buildProducts();
-   	/* buildProductCards(); */
-};
+//window.onload = function() { 
+//	buildProducts();
+//   	/* buildProductCards(); */
+//};
 
 function buildProducts() {
     for (i = 0; i < urls.length; i++) {
-        products.push(new product("", "", 0, "", 0, [], {}, "product" + i, urls[i]["amazon"], urls[i]["newegg"]));
+//    					  product(brand, model, price, imageUrl, averageRating, reviews, specifications, id, amazonUrl, neweggUrl)
+        products.push(new product("", "", 0, "", 0, [], {}, "product" + i, urls[i]["amazon"], urls[i]["newegg"], i));
         bsService.loadMetadata(urls[i]["amazon"], {}, amazon);
         bsService.loadMetadata(urls[i]["newegg"], {}, newegg);
     }
@@ -70,6 +72,7 @@ function buildProducts() {
 function amazon(err, metadataAndMetametaData)
 {
 	var unwrappedMetadata = BSUtils.unwrap(metadataAndMetametaData.metadata);
+    console.log(unwrappedMetadata);
     
     var i = 0;
     for (i; i < products.length; i++)
@@ -199,19 +202,21 @@ function buildProductCard(product) {
     container.appendChild(starAndPriceContainer);
     productCard.appendChild(container);
         
-    productCard.oncontextmenu = (function() {
-        var currentId = productCard.id;
-        return function() {
-            openCircularMenu(currentId + "Menu");
-        }
-    })();
+//    productCard.oncontextmenu = (function() {
+//        var currentId = productCard.id;
+//        return function() {
+//            openCircularMenu(currentId + "Menu");
+//        }
+//    })();
     
     
     var productCardId = productCard.id;
     productCards[productCardId] = productCard;
     
-    
-    productsContainer.appendChild(productCard);
+    var productCardLink = document.createElement("a");
+    productCardLink.href = "productPage.html?index=" + product.index;
+    productCardLink.appendChild(productCard);
+    productsContainer.appendChild(productCardLink);
     	
 }
 
@@ -224,3 +229,63 @@ function addTooltip(element, tooltipText) {
 	
 	element.appendChild(tooltipTextElement);
 }
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+$(document).ready(function () {
+    $('#checkbox1').change(function () {
+        if (!this.checked) 
+        //  ^
+           $('#autoCNET').fadeOut('slow');
+        else 
+            $('#autoCNET').fadeIn('slow');
+    });
+});
+$(document).ready(function () {
+    $('#checkbox2').change(function () {
+        if (!this.checked) 
+        //  ^
+           $('#autoDigit').fadeOut('slow');
+        else 
+            $('#autoDigit').fadeIn('slow');
+    });
+});
+$(document).ready(function () {
+    $('#checkbox3').change(function () {
+        if (!this.checked) 
+        //  ^
+           $('#autoSoftonic').fadeOut('slow');
+        else 
+            $('#autoSoftonic').fadeIn('slow');
+    });
+});
+$(document).ready(function () {
+    $('#checkbox4').change(function () {
+        if (!this.checked) 
+        //  ^
+           $('#autoReview').fadeOut('slow');
+        else 
+            $('#autoReview').fadeIn('slow');
+    });
+});
+$(document).ready(function () {
+    $('#checkbox5').change(function () {
+        if (!this.checked) 
+        //  ^
+           $('#autoProReviews').fadeOut('slow');
+        else 
+            $('#autoProReviews').fadeIn('slow');
+    });
+});
